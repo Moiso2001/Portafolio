adc454ad3a5e4f9071d6f8edfe22043d918519f9
MoisoDavid <wollxi@gmail.com>
2023-01-27 23:25:08 -0500

    SearchAlmostDone

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
+import styles from "./Search.module.css";
+
 export default function Search() {
   return (
-    <div>Search</div>
+    <div className={styles.divGlobal}>
+      <div className={styles.divFullWindow}>
+        <div className={styles.divInput}>
+          <div className={styles.divDomain}>
+            <span className={styles.domain}>localhost:3001</span>
+          </div>
+          <div className={styles.divRoutes}>
+            <input className={styles.routes} name='route' placeholder='/home'/>
+          </div>
+        </div>
+        <div className={styles.divOutput}>
+          <span className={styles.output}></span>
+        </div>
+      </div>
+    </div>
   )
 }
